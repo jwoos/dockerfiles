02d5e9208382c557e9167cc1b7520fe92c10abbb
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-07-06 15:55:50 -0400

    managing script

---
+++ manage.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+COMMAND=$1
+DIRECTORY=$2
+
+function run() {
+	docker run -it --rm \
+		--name "${DIRECTORY}-standalone" \
+		"tmp_${DIRECTORY}"
+}
+
+function build() {
+	docker build "${DIRECTORY}" -t "tmp_${DIRECTORY}"
+}
+
+function attach() {
+	docker exec -it $(docker ps -q -f name="${DIRECTORY}-standalone") bash
+}
+
+function _kill() {
+	docker kill "${DIRECTORY}-standalone"
+}
+
+if [[ -z $COMMAND ]]; then
+	echo 'You must specify a command! [run | build | attach]'
+	exit 1
+fi
+
+if [[ -z $DIRECTORY ]]; then
+	echo 'You must specify a directory!'
+	exit 1
+fi
+
+if [[ $COMMAND == 'run' ]]; then
+	run
+elif [[ $COMMAND == 'build' ]]; then
+	build
+elif [[ $COMMAND == 'attach' ]]; then
+	attach
+elif [[ $COMMAND == 'kill' ]]; then
+	_kill
+else
+	echo 'Invalid command'
+	exit 1
+fi
