1c64ed8b2cc60c222efbf0d2e89eefdcaffdacfc
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-07-12 14:23:29 -0400

    aws-lambda-python-3

---
+++ aws-lambda/python/3/index.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+def lambda_handler(event, context):
+    print('event:', event)
+
+    return 'It works!'
---
+++ aws-lambda/python/3/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+	--name aws-lambda-python-3-standalone \
+	tmp_aws-lambda-python-3
+
