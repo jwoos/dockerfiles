2fee787207eb66ea00e01dc2ae51d61fd3479545
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-07-13 17:09:17 -0400

    aws-lambda packagers

---
+++ aws-lambda/javascript/packager/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+	--name aws-lambda-javascript-packager-standalone \
+	tmp_aws-lambda-javascript-packager
---
+++ aws-lambda/python/packager/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+	--name aws-lambda-python-packager-standalone \
+	tmp_aws-lambda-python-packager
